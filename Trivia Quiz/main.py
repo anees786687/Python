5625e4dc1d4e7b76bb113398a06d8a0e4148bf94
anees786687 <78203712+anees786687@users.noreply.github.com>
2024-10-02 20:26:57 +0530

    Update main.py
    The game has an API integration of Open Trivia DB. The api_call module sends an API request to Open Trivia DB and fetches the True/False questions in form of json and parses it to a list of dictionaries. The user gets a dynamic experience where they can select how many questions they want and what is the difficulty level, these parameters are passed to the API url to fetch the required data.

--- Trivia Quiz/main.py
+++ Trivia Quiz/main.py
@@ -1,13 +1,17 @@
-from data import question_data
+# from data import question_data
 from question_model import QuestionModel
-import random
 from  quiz_brain import QuizBrain
-
+from api_call import GenerateQuestions
 # make a list on Question objects
+num_ques = int(input("Input the number of question: "))
+level = input("Choose the difficulty level (easy/medium/hard): ").lower()
+
+gq = GenerateQuestions(num_ques,level)
+question_data = gq.generate_question_bank()
 question_bank = []
 
 for question in question_data:
-    question_bank.append(QuestionModel(question["text"],question["answer"]))
+    question_bank.append(QuestionModel(question["question"],question["correct_answer"]))
 
 quiz = QuizBrain(question_bank)
 
